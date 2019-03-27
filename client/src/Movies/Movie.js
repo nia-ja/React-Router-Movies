c47934d963633f93aac3be16030a59c8d8a629d5
Kasia Bondarava <kasia.bondarava@gmail.com>
2019-03-27 12:24:05 -0700

    Stretch2: Added SaveMovie functionality, changed SaveMovie Links into NavLinks

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -26,23 +26,23 @@ export default class Movie extends Component {
       });
   };
   // Uncomment this code when you're ready for the stretch problems
-  // componentWillReceiveProps(newProps){
-  //   if(this.props.match.params.id !== newProps.match.params.id){
-  //     this.fetchMovie(newProps.match.params.id);
-  //   }
-  // }
+  componentWillReceiveProps(newProps){
+    if(this.props.match.params.id !== newProps.match.params.id){
+      this.fetchMovie(newProps.match.params.id);
+    }
+  }
 
-  // saveMovie = () => {
-  //   const addToSavedList = this.props.addToSavedList;
-  //   addToSavedList(this.state.movie)
-  // }
+  saveMovie = () => {
+    const addToSavedList = this.props.addToSavedList;
+    addToSavedList(this.state.movie)
+  }
 
-  render() {
+  render(props) {
     if (!this.state.movie) {
       return <div>Loading movie information...</div>;
     }
     return (
-      <MovieCard movie={this.state.movie} />
+      <MovieCard movie={this.state.movie} saveMovie={this.saveMovie} />
     );
   }
 }
