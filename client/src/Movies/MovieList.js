ec6c1d19f8d69d572615715127f3d55802165ef7
Kasia Bondarava <kasia.bondarava@gmail.com>
2019-03-27 11:52:03 -0700

    Stretch1: Refactored the code, created Movie component, made MovieList and MovieCard components both use Movie component not there own but exactly the same code

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 
+import MovieCard from './MovieCard';
+
 export default class MovieList extends Component {
   constructor(props) {
     super(props);
@@ -26,32 +28,10 @@ export default class MovieList extends Component {
         <div className="movie-list">
           {this.state.movies.map(movie => (
             <Link to={`/movies/${movie.id}`}>
-              <MovieDetails key={movie.id} movie={movie} />
+              <MovieCard key={movie.id} movie={movie} />
             </Link>
           ))}
         </div>
     );
   }
-}
-
-function MovieDetails({ movie }) {
-  const { title, director, metascore, stars } = movie;
-  return (
-    <div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-      <h3>Actors</h3>
-
-      {stars.map(star => (
-        <div key={star} className="movie-star">
-          {star}
-        </div>
-      ))}
-    </div>
-  );
 }
